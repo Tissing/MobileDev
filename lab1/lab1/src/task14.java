a439650870f768e796c1a8ff32c6b2f0d2d59cc0
MrTissing <shibanov-slava@mail.ru>
2024-04-05 19:47:11 +1100

    add some tasks

--- lab1/lab1/src/task14.java
+++ lab1/lab1/src/task14.java
@@ -1,6 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
 
 public class task14 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Reader reader = new FileReader("C:\\Users\\user\\Desktop\\Visual Studio\\MobileDev\\lab1\\lab1\\materials\\file_for_code.txt");
+        File file = new File("C:\\Users\\user\\Desktop\\Visual Studio\\MobileDev\\lab1\\lab1\\materials\\new_file.txt");
+        file.createNewFile();
+        FileOutputStream out = new FileOutputStream("C:\\Users\\user\\Desktop\\Visual Studio\\MobileDev\\lab1\\lab1\\materials\\new_file.txt");
+        int bt = reader.read();
+        int save = bt;
+        byte count = 1;
+        while((bt = reader.read()) != -1) {
+            System.out.println(Arrays.toString(Character.toChars(bt)) + " " + bt);
+            if (bt == save) {
+                count++;
+            } else {
+                out.write(count);
+                out.write(save);
+                System.out.println("Запись " + save + " "+ count);
+                save = bt;
+                count = 1;
+
+            }
+        }
+        out.write(count);
+        out.write(save);
+        out.close();
+        reader.close();
+        System.out.println("Запись " + save + " "+ count);
 
     }
 }
--- lab1/lab1/src/task9.java
+++ lab1/lab1/src/task9.java
@@ -3,14 +3,12 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class task9 {
-    public static void main(String[] args){
-        int inf = Integer.MAX_VALUE;
-        HashMap<Integer, Integer> dictNodes = new HashMap<>();
+    public static void main(String[] args) {
+        double inf = Double.POSITIVE_INFINITY;
+        HashMap<Integer, Double> dictNodes = new HashMap<>();
         int[] nodes = new int[]{1, 2, 3, 4, 5, 6};
-        for(int i: nodes){
-            dictNodes.put(i, inf);
-        }
-        ArrayList<Integer> completeNodes = new ArrayList<>(){};
+        for (int i : nodes) dictNodes.put(i, inf);
+        ArrayList<Integer> completeNodes = new ArrayList<>();
         int[][] edges = new int[][]{
                 {0, 1, 1, 0, 0, 1},
                 {1, 0, 1, 1, 0, 0},
@@ -18,9 +16,9 @@ public class task9 {
                 {0, 1, 1, 0, 1, 0},
                 {0, 0, 0, 1, 0, 1},
                 {1, 0, 1, 0, 1, 0}
-                };
-        int[][] weights = new int[][]{
-                {0, 7, 9, inf, inf, inf, 6},
+        };
+        double[][] weights = new double[][]{
+                {0, 7, 9, inf, inf, 14},
                 {7, 0, 10, 15, inf, inf},
                 {9, 10, 0, 11, inf, 2},
                 {inf, 15, 11, 0, 6, inf},
@@ -28,22 +26,39 @@ public class task9 {
                 {14, inf, 2, inf, 9, 0}
         };
         Scanner scanner = new Scanner(System.in);
-        int startNode = Integer.parseInt(scanner.nextLine());
-        int endNode = Integer.parseInt(scanner.nextLine());
+        int startNode = 1;//Integer.parseInt(scanner.nextLine());
+        dictNodes.put(startNode, 0.0);
+        int endNode = 4;//Integer.parseInt(scanner.nextLine());
+        ArrayList<Integer> path = new ArrayList<>();
         int currentNode = startNode;
-        while (completeNodes.size() != nodes.length){
-            int min = currentNode;
-            for(int i = 0; i < edges[currentNode - 1].length; i++){
-                if (edges[currentNode -1][i] == 1 && dictNodes.get(i + 1) > weights[currentNode - 1][i]){
-                    dictNodes.put(i + 1, weights[currentNode - 1][i]);
-                    if(weights[currentNode - 1][min] > weights[currentNode - 1][i])
-                        min = i;
+        while(completeNodes.size() != nodes.length) {
+            double min = inf;
+            int min_ind = -1;
+            completeNodes.add(currentNode);
+            for (int i = 0; i < edges[currentNode - 1].length; i++) {
+                if (edges[currentNode - 1][i] == 1) {
+                    if (dictNodes.get(i + 1) > dictNodes.get(currentNode) + weights[currentNode - 1][i]) {
+                        dictNodes.put(i + 1, dictNodes.get(currentNode) + weights[currentNode - 1][i]);
+                        if (i + 1 == endNode ){
+                            path = (ArrayList<Integer>) completeNodes.clone();
+                        }
+                    }
+
                 }
             }
-            completeNodes.add(currentNode);
-            currentNode = min;
-        }
-        System.out.println(dictNodes);
+            for(int i: dictNodes.keySet()){
+                if(dictNodes.get(i) < min && !completeNodes.contains(i)){
+                    min = dictNodes.get(i);
+                    min_ind = i;
+                }
+            }
 
+            currentNode = min_ind;
+            System.out.println(dictNodes);
+        }
+        //System.out.println(completeNodes);
+        System.out.println("Узел " + endNode + " = " + dictNodes.get(endNode));
+        path.add(endNode);
+        System.out.println(path);
     }
 }
